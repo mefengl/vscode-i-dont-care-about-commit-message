db3bc1ddaa023c8ffc66f694c6e760675e31fa5d
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-06-27 01:22:20 +0800

    added progress bar for git commit and push commands

--- src/extension.ts
+++ src/extension.ts
@@ -6,44 +6,57 @@ let workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 const gitHelper = simpleGit(workspaceRoot);
 
 async function createCommitMessage(diff: string) {
-	const openaiKey = vscode.workspace.getConfiguration('gitCommitAI').get('openaiApiKey') as string;
-	const model = vscode.workspace.getConfiguration('gitCommitAI').get('model') as string;
+    const openaiKey = vscode.workspace.getConfiguration('gitCommitAI').get('openaiApiKey') as string;
+    const model = vscode.workspace.getConfiguration('gitCommitAI').get('model') as string;
 
-	const configuration = new Configuration({ apiKey: openaiKey });
-	const openai = new OpenAIApi(configuration);
+    const configuration = new Configuration({ apiKey: openaiKey });
+    const openai = new OpenAIApi(configuration);
 
-	const chatCompletion = await openai.createChatCompletion(
-		{
-			model: model,
-			messages: [
-				{
-					role: "system",
-					content: "only answer with single line of commit msg itself"
-				},
-				{
-					role: 'user',
-					content: `git diff:\n${diff}`
-				}],
-		});
+    const chatCompletion = await openai.createChatCompletion(
+        {
+            model: model,
+            messages: [
+                {
+                    role: "system",
+                    content: "only answer with single line of commit msg itself"
+                },
+                {
+                    role: 'user',
+                    content: `git diff:\n${diff}`
+                }],
+        });
 
-	return chatCompletion.data.choices[0].message?.content || '';
+    return chatCompletion.data.choices[0].message?.content || '';
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('gitCommitAI', async () => {
-		const diff = await gitHelper.diff();
-		const commitMsg = await createCommitMessage(diff);
-		await gitHelper.add('.').commit(commitMsg);
-		vscode.window.showInformationMessage('Commit Successful!');
-	}));
+    context.subscriptions.push(vscode.commands.registerCommand('gitCommitAI', async () => {
+        await vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            title: "Processing Git Commit",
+            cancellable: false
+        }, async () => {
+            const diff = await gitHelper.diff();
+            const commitMsg = await createCommitMessage(diff);
+            await gitHelper.add('.').commit(commitMsg);
+        });
+        vscode.window.showInformationMessage('Commit Successful!');
+    }));
 
-	context.subscriptions.push(vscode.commands.registerCommand('gitPushAI', async () => {
-		const diff = await gitHelper.diff();
-		const commitMsg = await createCommitMessage(diff);
-		const currentBranch = await gitHelper.revparse(['--abbrev-ref', 'HEAD']);
-		await gitHelper.add('.').commit(commitMsg).push('origin', currentBranch);
-		vscode.window.showInformationMessage('Push Successful!');
-	}));
+    context.subscriptions.push(vscode.commands.registerCommand('gitPushAI', async () => {
+        await vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            title: "Processing Git Push",
+            cancellable: false
+        }, async () => {
+            const diff = await gitHelper.diff();
+            const commitMsg = await createCommitMessage(diff);
+            const currentBranch = await gitHelper.revparse(['--abbrev-ref', 'HEAD']);
+            await gitHelper.add('.').commit(commitMsg).push('origin', currentBranch);
+        });
+        vscode.window.showInformationMessage('Push Successful!');
+    }));
 }
+
 
 export function deactivate() { }
